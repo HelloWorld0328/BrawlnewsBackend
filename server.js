cae2f5468a79051a9c3a7df3a0321197d51a476f
HelloWorld0328 <rdy00328@gmail.com>
2023-10-25 15:15:56 +0900

    corscorscors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const cors = require('cors')
 
 app.use(cors());
 // app.use((req, res, next) => {
