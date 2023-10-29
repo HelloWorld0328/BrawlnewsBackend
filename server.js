f3684e67b2f082f8c5e28ac2d19e905b88ec713e
HelloWorld0328 <rdy00328@gmail.com>
2023-10-29 09:37:14 +0900

    ededede

--- server.js
+++ server.js
@@ -3,23 +3,14 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const cors = require('cors')
+const cors = require('cors');
 
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://brawlnews.kro.kr/'); // 허용할 도메인을 설정합니다.
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
-
-app.use(bodyParser.json()); // JSON 데이터 파싱
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// JSON 파일 경로
 const jsonFilePath = __dirname + "/posts.json";
 
-// JSON 파일을 읽어오는 함수
 function readJson() {
   try {
     const data = fs.readFileSync(jsonFilePath, "utf-8");
@@ -29,70 +20,46 @@ function readJson() {
   }
 }
 
-// JSON 파일에 데이터를 저장하는 함수
 function writeJson(data) {
   fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2), "utf-8");
 }
 
-// 루트 경로
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// 모든 포스트 가져오기
 app.get('/posts', (req, res) => {
   const posts = readJson();
   res.json(posts);
 });
+
 app.get('/index', (req, res) => {
   const posts = readJson();
-  const datapost=posts
-  const len=datapost.length
-  res.send(len)
+  res.send(posts.length);
 });
 
-// 새로운 포스트 추가
 app.post('/upload', (req, res) => {
   let reqdata = req.body;
-  // JSON 파일에서 데이터 읽기
   const posts = readJson();
-  reqdata.id=readJson().length
-  console.log(reqdata);
-
-  // 새로운 데이터 추가
+  reqdata.id = posts.length;
   posts.push(reqdata);
-
-  // JSON 파일에 데이터 저장
   writeJson(posts);
-
   res.json({ message: `데이터가 성공적으로 추가되었습니다.,${reqdata}` });
 });
-app.get('/posts', (req, res) => {
-  const posts = readJson();
-  res.json(posts);
-});
-app.get('/index', (req, res) => {
-  const posts = readJson();
-  const datapost=posts
-  const len=datapost.length
-  res.send(len)
-});
 
-// 댓글
 app.post('/comment', (req, res) => {
   let reqdata = req.body;
-  // JSON 파일에서 데이터 읽기
-  let posts = readJson()
-  console.log(reqdata);
-  posts[reqdata.id].comment.push({name:reqdata.name,comment:reqdata.comment})
-
-  // 새로운 데이터 추가
-  postid.comment.push(posts);
-
-  // JSON 파일에 데이터 저장
-  writeJson(posts);
-
-  res.json({ message: `데이터가 성공적으로 추가되었습니다.,${reqdata}` });
+  const posts = readJson();
+  if (reqdata.id < posts.length) {
+    if (!posts[reqdata.id].comment) {
+      posts[reqdata.id].comment = [];
+    }
+    posts[reqdata.id].comment.push({ name: reqdata.name, comment: reqdata.comment });
+    writeJson(posts);
+    res.json({ message: `댓글이 성공적으로 추가되었습니다.,${reqdata}` });
+  } else {
+    res.status(400).json({ error: '유효하지 않은 ID입니다.' });
+  }
 });
 
 app.listen(port, () => {
